7921556a900467db72c393189dc788e226a66fcd
cyndiWade <zhanglin492103904@qq.com>
2013-11-19 17:46:13 +0800

    2013-11-19 17:46

--- App/Public/media/js/run.js
+++ App/Public/media/js/run.js
@@ -16,8 +16,8 @@
 	//$.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());		//IE
 	var check_browser = false;
 	var arr_browser = [
-			$.browser.msie10,
-			$.browser.msie9,
+			//$.browser.msie10,
+			//$.browser.msie9,
 			$.browser.mozilla,
 			$.browser.webkit,
 			$.browser.opera
@@ -44,22 +44,31 @@
 		var menu_children_li = menu_li.children().find('li');			//二级导航
 		var cookie = new Cookie();												//Cookie操作类
 	
-		var index = cookie.getCookie('menu_index') || 1;				//点击导航后获取的索引		
-		menu_li.eq(index -1 ).addClass('active');								//为点击的导航加上样式
+		var menu_index = cookie.getCookie('menu_index') || 1;				//点击一级导航后获取的索引		
+		var two_menu_index = cookie.getCookie('two_menu_index') || 0;	//点击二级导航后获取的索引
+
+		var one_menu_list = menu_li.eq(menu_index -1 );	//当前选中一级导航
+		one_menu_list.addClass('active');								//为点击的导航加上样式
+		var two_menu_list = one_menu_list.children().find('li');			//当前选中二级导航
+		two_menu_list.eq(two_menu_index).addClass('active');			//为点击样式加上首选
 		
 		//一级导航没有子导航时加样式
 		menu_li.click(function () {
 			var _this = $(this);
 			if (_this.children().is('ul') == false) {
-				cookie.setCookie('menu_index',_this.index());				//保存导航名索引到Cookie
+				cookie.setCookie('menu_index',_this.index());				//保存一级导航名索引到Cookie
 			}
 		});
 		
 		//二级导航选中后加样式
 		menu_children_li.click(function () {
 			var _this = $(this);
+			cookie.setCookie('two_menu_index',_this.index());			
 			cookie.setCookie('menu_index',_this.parent().parent().index());		//保存导航名到Cookie
 		});
+		
+		
+		
 		
 	})(jQuery);
 	
