9ae1760868dfff9c2a61dae269a3794d8f688975
cyndiWade <zhanglin492103904@qq.com>
2013-12-29 19:08:06 +0800

    wade 19:08

--- App/Lib/Model/Api/CarsModel.class.php
+++ App/Lib/Model/Api/CarsModel.class.php
@@ -12,13 +12,14 @@ class CarsModel extends ApiBaseModel {
 	}
 
 	
-	//获取账号数据
+	//获取可用车辆数据
 	public function seek_all_cars ($car_status) {
 		$con['status'] = 0;
 		$con['car_status'] = array('not in',$car_status);
 //		$con['cp.type'] = 1;
 		$data = $this->field('id,cars_grade_id,brand,car_num,type,model,color,consumption,seat_num,car_status')
 		->where($con)
+		->order('id ASC')
 		->select();
 
 		return $data;
